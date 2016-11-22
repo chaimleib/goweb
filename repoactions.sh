245526ab41081cb7180224ea6e4bd0b70c9af386
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2016-11-22 14:21:01 -0800

    use repoactions

--- repoactions.sh
+++ repoactions.sh
@@ -2,6 +2,9 @@
 # This file is to be source-d when entering this directory. It sets up
 # project-specific environment variables, such as interpreter versions and
 # import paths.
+# https://github.com/chaimleib/repoactions
+
 gitroot="$(git rev-parse --show-toplevel)"
 export GOPATH="$gitroot"
+
 
