4db052bf7917e628de1b321811659e5e875189f2
Chaim-Leib Halbert <chaim.leib.halbert@gmail.com>
2016-11-23 17:59:53 -0800

    explain why not using prependPath for GOPATH

--- repoactions.sh
+++ repoactions.sh
@@ -5,11 +5,12 @@
 # https://github.com/chaimleib/repoactions
 
 gitroot="$(git rev-parse --show-toplevel)"
+BASH_LIBS="${gitroot}/bash" source bash/pathfuncs.sh
 
 # import path for golang
+#prependPath "${gitroot}/src/go" GOPATH
+# make sure to erase any other import paths:
 export GOPATH="${gitroot}/src/go"
 
 # put our golang tools in our PATH
-BASH_LIBS="${gitroot}/bash" source bash/pathfuncs.sh
 prependPath "${GOPATH}/bin"
-
